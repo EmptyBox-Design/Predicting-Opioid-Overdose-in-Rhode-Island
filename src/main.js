e53f5debf1d2e76a7bb820cd919914fabd86ad9e
Emptybox-Design <bpachuca1@gmail.com>
2021-08-01 15:36:10 -0600

    mapbox css import

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// BOOTSTRAP
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// MAPBOX
+import 'mapbox-gl/dist/mapbox-gl.css'
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
