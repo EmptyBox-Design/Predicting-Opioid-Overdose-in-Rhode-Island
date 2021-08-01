a7ab77456760a656128d5f7d7e9ed71eaa327742
Emptybox-Design <bpachuca1@gmail.com>
2021-08-01 15:22:34 -0600

    deploy

--- dist/js/about.75a6be40.js
+++
@@ -1,2 +0,0 @@
-(window['webpackJsonp'] = window['webpackJsonp'] || []).push([['about'], { f820: function (t, e, n) { 'use strict'; n.r(e); var a = function () { var t = this; var e = t.$createElement; t._self._c; return t._m(0) }; var s = [function () { var t = this; var e = t.$createElement; var n = t._self._c || e; return n('div', { staticClass: 'about' }, [n('h1', [t._v('This is an about page')])]) }]; var u = n('2877'); var c = {}; var i = Object(u['a'])(c, a, s, !1, null, null, null); e['default'] = i.exports } }])
-// # sourceMappingURL=about.75a6be40.js.map
